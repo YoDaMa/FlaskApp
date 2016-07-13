2c814c0c219bd23468f3f8c8e3381ff26311890c
YoDaMa <yoseph.maguire@gmail.com>
2016-07-13 14:03:52 -0500

    Cleaned Up
    Cleaned up the app for further development on stuff.

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
-
+# Not using forms yet so it's okay to not worry about this...
 class LoginForm(Form):
 	openid = StringField('oepnid', validators=[DataRequired()])
 	remember_me = BooleanField('remember_me', default=False)
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!flask/bin/python
-from app import app
+from myapp import app
 app.run(debug=True)
